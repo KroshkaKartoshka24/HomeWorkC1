ceb1cb28f54ce8e10a85abcd6916854aa65d4207
Christina <nemovakg@gmail.com>
2023-09-22 19:52:55 +0300

    Задача 13

--- Program.cs
+++ Program.cs
@@ -3,13 +3,29 @@
 // 782 -> 8
 // 918 -> 1
 
+// System.Console.WriteLine("Введите трехзначное число: ");
+// string num = System.Console.ReadLine();
+// if (num.Length == 3)
+// {
+//     System.Console.WriteLine("Вторая цифра введенного числа: " + num.Substring (1, 1));
+// }
+// else 
+// {
+//     System.Console.WriteLine("Введенное число не соответствует условиям задачи!");
+// }
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
 System.Console.WriteLine("Введите трехзначное число: ");
 string num = System.Console.ReadLine();
-if (num.Length == 3)
+if (num.Length >= 3)
 {
-    System.Console.WriteLine("Вторая цифра введенного числа: " + num.Substring (1, 1));
+    System.Console.WriteLine("Третья цифра введенного числа: " + num.Substring (2, 1));
 }
 else 
 {
-    System.Console.WriteLine("Введенное число не соответствует условиям задачи!");
+    System.Console.WriteLine("Третья цифра отсутствует");
 }
